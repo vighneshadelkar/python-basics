3d9453bd3d0d2221cc371dcc4091536e7ca96000
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2025-01-07 02:37:13 +0530

    Create math_module.py

---
+++ math_module.py
@@ -0,0 +1,18 @@
+# in-built methods or modules
+
+import math
+
+print(math.sqrt(16))
+print(math.pow(2,5))
+
+# to know all commends in math module
+print(dir(math))
+print(math.pi)
+print(math.log10(1000))
+print(math.ceil(12.3))
+
+import calender
+
+cal=calender.month(2016,1)
+calender.isleap(2015)
+print(cal)
