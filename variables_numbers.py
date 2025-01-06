58d55670757ca90b44af89db3ee7c997951c4d8c
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2025-01-06 23:53:41 +0530

    Create variables_numbers.py

---
+++ variables_numbers.py
@@ -0,0 +1,30 @@
+# variables
+rent=1220
+gas=202.5
+groceries=305.6
+
+total=rent+gas+groceries
+print(total)
+
+# we can re assign value to a variable
+rent = 1400
+
+item1="rent"
+
+print("item1",item1)
+
+# cannot use predefined keywords as variable name
+
+
+# there are two types of numbers 1)integer 2)float
+# for power of a number
+power=2**3
+print(power)
+
+# round off a number
+div=435/65
+
+print(div)
+print(round(div,2),round(div,3))
+
+# there is no precise way to store float numbers
