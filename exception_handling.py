3e5d2e2b4ba01d4e02037582ab53a7f24d8a8f35
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2025-01-08 18:12:02 +0530

    Update exception_handling.py

--- exception_handling.py
+++ exception_handling.py
@@ -11,3 +11,34 @@ except Exception as e:
     
 print(div)
     
+   
+
+# raise exception
+    
+try:
+    raise Exception("exception")
+except Exception as e:
+    print(e)
+
+try:
+    raise MemoryError("memory error")
+except MemoryError as e:
+    print(e)
+finally:
+    # used to do cleanup
+    print("code will run even if there is an exception")
+    
+# user defined exception
+
+class Accident(Exception):
+    def __init__(self,msg):
+        self.msg=msg
+        
+    def print_exception(self):
+        print(self.msg)
+
+
+try:
+    raise Accident("user defined exception")
+except Accident as e:
+    e.print_exception()
